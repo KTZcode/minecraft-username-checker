cf38d4e6c6368c77058d39212d45194b99f24696
Kqzz <channelktz@gmail.com>
2020-06-19 12:53:54 -0400

    I don't know what I did lol but its way better...
    Updated experimental_username_checker.py to be better and updated README.md to explain new changes. corrected grammar too.

--- README.md
+++ README.md
@@ -3,19 +3,37 @@
 This simple Python script will check, from a list of names, if any of them are available. If any are, it will open them in [namemc.com](https://namemc.com/) (if you want them to) on a new tab in your web browser for you to confirm if they are truly available or just "available soon."
 
 ### Requirements:
-- [python](https://www.python.org/)
+- [Python](https://www.python.org/)
 - [Python requests module](https://requests.readthedocs.io/en/master/)
 - [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) (for `experimental_username_checker.py`)
 
 
 ### usage:
 - put a list of names you want to check in the "names.txt" file (there is no limit but if you have it open the names on namemc you might run out of ram.)
-- add `{"open_name_in_namemc": true or false}` to a file named `config.json` file in the root directory of this project (This will decided whether or not the names will be opened on namemc.)
+- add
+```json
+    {
+        "open_name_in_namemc": true or false,
+        "min_likes_to_save": integer,
+        "save_available_later_names": true or false
+    }
+```
+ to a file named `config.json` file in the root directory of this project (This will decided whether or not the names will be opened on namemc.) Note: "min_likes_to_save" and "save_available_later_names" only matter for `experimental_username_checker.py` because I have not implemented it into `username_ckecker.py`.
 -  run `username_checker.py` or `experimental_username_checker.py` and it will output to `namelog.txt`.
 
 ### Other info:
-`experimental_username_checker.py` will require [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) to run because it will scrape data (*True* availability and searches / month) from [namemc](https://namemc.com/), print it out, and write it in `namelog.txt`. It *is* slower but I recommend it when checking a large number of names.
+I've only really left `username_ckecker.py` because it is so much faster but I strongly encourage using `experimental_username_checker.py` because it can be used with more effectively with a large number of names.  `experimental_username_checker.py` will require [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) to run because it will scrape data (*True* availability and searches / month) from [namemc](https://namemc.com/), print it out, and write it in `namelog.txt`. It *is* slower but I recommend it when checking a large number of names.
 If you have any issues with the script please open an issue and I will try to get back as soon as possible.
+### config examples
+If I were to run this script using [the set of words below](https://github.com/dwyl/english-words) then I would recommend running `experimental_username_checker.py` with a config like this:
+```json
+    {
+        "open_name_in_namemc": false,
+        "min_likes_to_save": 1,
+        "save_available_later_names": true
+    }
+```
+because you don't want that many names opening in namemc, most of the names will be bad so you want to ensure that they get at least 1 search per month, and that the available soon names are saved because there will be very few of those which probably will become available and not get sniped quickly. Or, if you are using a set of common or short names then you might want a config where it doesn't save "available later" names because there would be a lot of those and you are just looking for the currently available.
 
 #### Resources:
 For finding words to check I recommend [this](https://github.com/dwyl/english-words) GitHub repository because there are a *TON* of words and they are already correctly formatted.
--- experimental_username_chcker.py
+++ experimental_username_chcker.py
@@ -4,45 +4,71 @@ import webbrowser
 from time import sleep
 import json
 from bs4 import BeautifulSoup
-
+import datetime
 
 configs = json.load(open("config.json"))
 if configs["open_name_in_namemc"] == True:
     print(f"names will be opening in namemc this session")
 if configs["open_name_in_namemc"] == False:
     print(f"names will not be opening in namemc this session")
-sleep(1)
+
+available_later = 0
 failed_names = 0
 available_names = 0
 name_in = open("names.txt", "r")
 name_out = open("namelog.txt", "a")
 names = name_in.readlines()
 converted_names = []
+sleep(1)
+
+
 for element in names:
     converted_names.append(element.strip())
+
+number_of_names = len(converted_names)
+print(f"checking {number_of_names} names starting now")
+
 def name_is_available(name):
-    global failed_names, available_names
+    global failed_names, available_names, available_later
     url = f"https://namemc.com/search?q={name}"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    try:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+    except:
+        print("--- could not get webpage from namemc, pausing for 1 minute. ---")
+        sleep(60)
+        name_is_available(name)
     status_bar = soup.find(id="status-bar")
     if str(type(status_bar)) == "<class 'NoneType'>":
-        print("there was an error, stopping temporarily. This is normal.")
-        sleep(30)
+        print("---- Too many requests have been sent to namemc, pausing for 40 seconds. ----")
+        sleep(40)
         name_is_available(name)
         return
     info = status_bar.find_all("div", class_="col-sm-6 my-1")
-    searches_per_month = info[1].text.split("\n")[2]
+    searches_per_month = info[1].text.split("\n")[2].split(" ")[0]
     status = info[0].text.split("\n")[2]
+    if int(searches_per_month) >= configs["min_likes_to_save"]:
+        do_not_include = ["unavailable", "too short", "too long", "invalid characters"]
+        if configs["save_available_later_names"] == False:
+            do_not_include.append("available later*")
+        if status.lower() not in do_not_include:
+            available_names += 1
+            name_out.write(f"{name} is: {status} and has {searches_per_month} searches per month\n")
+        elif status.lower() == "available later*":
+            available_later += 1
     print(f"{name} is: {status} and has {searches_per_month} searches per month")
-    name_out.write(f"{name} is: {status} and has {searches_per_month} searches per month\n")
     if configs["open_name_in_namemc"] == True:
         webbrowser.open(f"https://namemc.com/search?q={name}")
-    available_names += 1
+
 
 def full_loop(name):
-    global failed_names, available_names
-    r = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{name}")
+    global failed_names, available_names, available_later
+    try:
+        r = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{name}")
+    except:
+        print("--- could not get data from mojang, pausing for 1 minute and trying again ---")
+        sleep(60)
+        r = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{name}")
     try:
         username_json = json.loads(r.text)
         if username_json == {'error': 'TooManyRequestsException', 'errorMessage': 'The client has sent too many requests within a certain amount of time'}:
@@ -52,16 +78,25 @@ def full_loop(name):
             print("waited for 10 minutes")
             sleep(1)
         elif username_json != {'error': 'TooManyRequestsException', 'errorMessage': 'The client has sent too many requests within a certain amount of time'}:
-            print(username_json)
+            print(f"{username_json['name']} is unavailable")
             failed_names += 1
     except json.decoder.JSONDecodeError:
         name_is_available(name)
 
+
 for name in converted_names:
     full_loop(name)
 
+
 success_rate = f"{available_names / (available_names + failed_names) * 100}%"
-print(f"------ you were {success_rate} succesful this time ------")
-name_out.write(f"------ you were {success_rate} succesful this time ------")
+available_later_rate =  f"{available_later / number_of_names}%"
+print(f"\n\n------ you were {success_rate} succesful this time. ------")
+print(f"{available_later_rate} of the names were available later")
+print(f"checked {number_of_names} name(s).")
+print(f"finished at {datetime.datetime.now().replace(microsecond=0)}.")
+name_out.write(f"\n\n------ finished at {datetime.datetime.now().replace(microsecond=0)}. ------\n")
+name_out.write(f"you were {success_rate} succesful this time.\n")
+name_out.write(f"checked {number_of_names} names.")
+
 
 name_out.close()
